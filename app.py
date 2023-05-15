3e716bd5adc8e5437c1add929f4301ebf478a85c
gVICKOS <90777936+gVICKOS@users.noreply.github.com>
2023-05-15 22:13:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import joblib
+import pickle
 from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
